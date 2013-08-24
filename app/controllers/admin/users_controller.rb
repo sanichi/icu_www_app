152ab4ddcdbc9e978c1a086a491b502d262534ea
Mark Orr <mark.j.l.orr@googlemail.com>
2013-08-24 19:16:28 +0100

    Started preparing views for users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,58 +1,50 @@
 class Admin::UsersController < ApplicationController
-  before_action :set_admin_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /admin/users
   def index
-    @admin_users = Admin::User.all
+    @users = User.all
   end
 
-  # GET /admin/users/1
   def show
   end
 
-  # GET /admin/users/new
   def new
-    @admin_user = Admin::User.new
+    @user = User.new
   end
 
-  # GET /admin/users/1/edit
   def edit
   end
 
-  # POST /admin/users
   def create
-    @admin_user = Admin::User.new(admin_user_params)
+    @user = User.new(admin_user_params)
 
-    if @admin_user.save
-      redirect_to @admin_user, notice: 'User was successfully created.'
+    if @user.save
+      redirect_to @user, notice: "User was successfully created"
     else
       render action: 'new'
     end
   end
 
-  # PATCH/PUT /admin/users/1
   def update
-    if @admin_user.update(admin_user_params)
-      redirect_to @admin_user, notice: 'User was successfully updated.'
+    if @user.update(user_params)
+      redirect_to @user, notice: "User was successfully updated"
     else
-      render action: 'edit'
+      render action: "edit"
     end
   end
 
-  # DELETE /admin/users/1
   def destroy
-    @admin_user.destroy
-    redirect_to admin_users_url, notice: 'User was successfully destroyed.'
+    @user.destroy
+    redirect_to admin_users_url, notice: "User was successfully destroyed"
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_admin_user
-      @admin_user = Admin::User.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def admin_user_params
-      params.require(:admin_user).permit(:email, :password, :salt, :icu_id, :expiry)
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:admin_user).permit(:email, :password, :salt, :icu_id, :expiry)
+  end
 end
---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,2 @@
+Time::DATE_FORMATS[:html5] = "%Y-%m-%dT%H:%M:%SZ"
+Time::DATE_FORMATS[:nosec] = "%Y-%m-%d %H:%M"
