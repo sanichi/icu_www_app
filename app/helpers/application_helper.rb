6476be5eebe4a6b626a501e75e07f0eac9eefc05
Mark Orr <mark.j.l.orr@googlemail.com>
2013-08-28 11:52:54 +0100

    Improved bootstrap usage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def active(controller, action=nil, active="active", inactive="")
+    boolean = params[:controller] == controller
+    boolean = boolean && params[:action] == action if action
+    boolean ? active : inactive
+  end
 end
