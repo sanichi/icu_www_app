118e8d674dccdfbbe0b1d76726bfdd7e1f48855b
Mark Orr <mark.j.l.orr@googlemail.com>
2013-09-27 17:45:51 +0100

    Correction 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ IcuWwwApp::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += User::THEMES.map { |theme| "#{theme.downcase}.min.css"}
+  config.assets.precompile += User::THEMES.reject{ |theme| theme == "Bootstrap" }.map{ |theme| "#{theme.downcase}.min.css"}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
