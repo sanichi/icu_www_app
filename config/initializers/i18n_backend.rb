2be295d4a3aa6259a0c5225b46a277d973e10d79
Mark Orr <mark.j.l.orr@googlemail.com>
2013-10-02 23:07:14 +0100

    Revert experiment to try to fix deployment problem

--- config/initializers/i18n_backend.rb
+++ config/initializers/i18n_backend.rb
@@ -1,5 +1,5 @@
 # Simple for English and KeyValue for Irish.
-#I18n.backend = I18n::Backend::Chain.new(I18n::Backend::Simple.new, I18n::Backend::KeyValue.new(Translation, false))
+I18n.backend = I18n::Backend::Chain.new(I18n::Backend::Simple.new, I18n::Backend::KeyValue.new(Translation, false))
 
 # Except for the test environment, update the DB translations to reflect changes in the YAML files.
-#Translation.update_db unless Rails.env == "test"
+Translation.update_db unless Rails.env == "test"
