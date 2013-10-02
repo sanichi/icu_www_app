db8ec23d48b132fd685de8d9d109631aa4a757c8
Mark Orr <mark.j.l.orr@googlemail.com>
2013-10-02 23:30:14 +0100

    Final fix for translations table deploy problem

--- config/initializers/i18n_backend.rb
+++ config/initializers/i18n_backend.rb
@@ -1,5 +1,8 @@
-# Simple for English and KeyValue for Irish.
-I18n.backend = I18n::Backend::Chain.new(I18n::Backend::Simple.new, I18n::Backend::KeyValue.new(Translation, false))
+# Only works if the translations table already exists.
+if ActiveRecord::Base.connection.table_exists? "translations"
+  # Simple for English and KeyValue for Irish.
+  I18n.backend = I18n::Backend::Chain.new(I18n::Backend::Simple.new, I18n::Backend::KeyValue.new(Translation, false))
 
-# Except for the test environment, update the DB translations to reflect changes in the YAML files.
-Translation.update_db unless Rails.env == "test"
+  # Except for the test environment, update the DB translations to reflect changes in the YAML files.
+  Translation.update_db unless Rails.env == "test"
+end
