36f87e14be3700ed1fa2b7541eaaf4162f588df6
Mark Orr <mark.j.l.orr@googlemail.com>
2013-10-17 09:39:06 +0100

    Made this test actually useful

--- spec/concerns/journalable_spec.rb
+++ spec/concerns/journalable_spec.rb
@@ -1,23 +1,28 @@
 require 'spec_helper'
 
 describe Journalable do
+  def invalid(klass)
+    valid = klass.column_names
+    klass.journalable_columns.reject { |column| valid.include?(column) }.join(" ")
+  end
+
   context Club do
     it "should be setup correctly" do
-      expect(Club.journalable_columns).to eq Set.new(%w[name web meet address district city county lat long contact email phone active])
+      expect(invalid(Club)).to be_blank
       expect(Club.journalable_path).to eq "/clubs/%d"
     end
   end
 
   context Translation do
     it "should be setup correctly" do
-      expect(Translation.journalable_columns).to eq Set.new(%w[value])
+      expect(invalid(Translation)).to be_blank
       expect(Translation.journalable_path).to eq "/admin/translations/%d"
     end
   end
 
   context User do
     it "should be setup correctly" do
-      expect(User.journalable_columns).to eq Set.new(%w[status encrypted_password roles])
+      expect(invalid(User)).to be_blank
       expect(User.journalable_path).to eq "/admin/users/%d"
     end
   end
