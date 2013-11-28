f5ecd2fe90dcb470b3e73ba2504d32351ecb4b22
Mark Orr <mark.j.l.orr@googlemail.com>
2013-11-28 14:40:07 +0000

    Improved the search menu for Journal Entries

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   ROLES = %w[admin editor membership translator treasurer]
   MINIMUM_PASSWORD_LENGTH = 6
   THEMES = %w[Bootstrap Cerulean Cosmo Cyborg Flatly Journal Readable Simplex Slate Spacelab]
-  DEFAULT_THEME = "Flatley"
+  DEFAULT_THEME = "Flatly"
   LOCALES = %w[en ga]
   SessionError = Class.new(RuntimeError)
 
