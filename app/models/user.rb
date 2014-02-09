7239f460cc2aed164fa7a79eca1cb159eb1bdbc9
Mark Orr <mark.j.l.orr@googlemail.com>
2014-02-09 18:49:26 +0000

    Upgrade to bootstrap and added some new themes

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   OK = "OK"
   ROLES = %w[admin editor membership translator treasurer]
   MINIMUM_PASSWORD_LENGTH = 6
-  THEMES = %w[Bootstrap Cerulean Cosmo Cyborg Flatly Journal Readable Simplex Slate Spacelab]
+  THEMES = %w[Amelia Bootstrap Cerulean Cosmo Cyborg Flatly Journal Lumen Superhero Readable Simplex Slate Spacelab United Yeti]
   DEFAULT_THEME = "Flatly"
   LOCALES = %w[en ga]
   SessionError = Class.new(RuntimeError)
