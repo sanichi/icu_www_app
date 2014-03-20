96353eaec22229fcee50909f8c6614424cd268d3
Mark Orr <mark.j.l.orr@googlemail.com>
2014-03-20 16:17:37 +0000

    More cleaning up after refactor

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,7 +15,7 @@ class ItemsController < ApplicationController
 
     if cart.does_not_already_have?(@item) && @item.save
       cart.items << @item
-      redirect_to cart_path(cart)
+      redirect_to cart_path
     else
       flash_first_base_error(@item)
       @item = @item.becomes(Item)
@@ -39,36 +39,3 @@ class ItemsController < ApplicationController
     params[:item].permit(:type, :fee_id, :player_id)
   end
 end
-
-# class EntriesController < ApplicationController
-#   def new
-#     @fee = EntryFee.on_sale.find_by(id: params[:entry_fee_id])
-#     redirect_to shop_path unless @fee
-#     @entry = Entry.new(entry_fee: @fee)
-#   end
-#
-#   def create
-#     @entry = Entry.new(entry_params)
-#     @fee = @entry.entry_fee
-#     @entry.description = @fee.description
-#     @entry.cost = @fee.cost
-#     @entry.event_start = @fee.event_start
-#     @entry.event_end = @fee.event_end
-#
-#     cart = current_cart(:create)
-#
-#     if @entry.save
-#       cart_item = CartItem.create(cart: cart, cartable: @entry)
-#       redirect_to cart_path(cart)
-#     else
-#       flash.now.alert = @entry.errors.to_a.first
-#       render "new"
-#     end
-#   end
-#
-#   private
-#
-#   def entry_params
-#     params[:entry].permit(:entry_fee_id, :player_id)
-#   end
-# end
