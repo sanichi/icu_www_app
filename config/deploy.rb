05b3e3a3a8a6f93c22a2bcc6e5b566317586c188
Mark Orr <mark.j.l.orr@googlemail.com>
2014-06-29 18:36:01 +0100

    Changed capistrano log level

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,22 +11,10 @@ set :linked_dirs, %w{log tmp/pids public/system}  # capistrano/rails adds public
 
 set :maintenance_file, "public/system/maintenance.html"
 
-# set :application, 'my_app_name'
-# set :repo_url, 'git@example.com:me/my_repo.git'
-
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-
-# set :deploy_to, '/var/www/my_app'
-# set :scm, :git
+set :log_level, :info
 
 # set :format, :pretty
-# set :log_level, :debug
 # set :pty, true
-
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 # set :keep_releases, 5
 
 # namespace :deploy do
