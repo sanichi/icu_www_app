6c9b19983191e169980a7554c60f4ff4bebb9836
Mark Orr <mark.j.l.orr@googlemail.com>
2014-07-31 11:45:12 +0100

    Experiment

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,3 @@
 # Be sure to restart your server when you modify this file.
-IcuWwwApp::Application.config.session_store :cookie_store, key: "_www_session"
+# Using domain => all so that sessions on www.icu.ie and icu.ie are shared.
+IcuWwwApp::Application.config.session_store :cookie_store, key: "_www_session", domain: :all
