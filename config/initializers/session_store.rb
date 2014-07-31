8aca45cd6b92405e25c44d3aad7935029f265e60
Mark Orr <mark.j.l.orr@googlemail.com>
2014-07-31 12:08:30 +0100

    Experiment 2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 # Using domain => all so that sessions on www.icu.ie and icu.ie are shared.
-IcuWwwApp::Application.config.session_store :cookie_store, key: "_www_session", domain: :all
+sess_opt = { key: "_www_session" }
+sess_opt[:domain] = ".icu.ie" if Rails.env.production?
+IcuWwwApp::Application.config.session_store :cookie_store, sess_opt
