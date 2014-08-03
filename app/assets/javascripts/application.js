e9c528ecfba2343b8b65b1dab853f5e6f8dfc7e7
Mark Orr <mark.j.l.orr@googlemail.com>
2014-08-03 17:07:42 +0100

    The email obscurer too common not to be included in the main javascript file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,3 +20,20 @@ $(function() {
     $(this).parents('form').submit();
   });
 });
+
+// For obscuring email addresses. Shpx gur fcnzzref.
+function liame() {
+  var a = new Array();
+  for (var i=0; i<arguments.length; i++)
+  {
+    a.push(elgnam(arguments[i]));
+  }
+  document.write(a.reverse().join('.'));
+}
+function elgnam(str)
+{
+  return str.replace(/[a-zA-Z]/g, function(c)
+  {
+    return String.fromCharCode((c<='Z'?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);
+  });
+}
