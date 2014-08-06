572b3016fccabcee83c23ce4e4eb80c62be12a7e
Mark Orr <mark.j.l.orr@googlemail.com>
2014-08-06 11:32:37 +0100

    Add new JS file to pre-compile list

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,4 @@ Rails.application.config.assets.version = "1.0"
 
 # Precompile additional assets. application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += User::THEMES.reject{ |theme| theme == "Bootstrap" }.map{ |theme| "#{theme.downcase}.min.css"}
-Rails.application.config.assets.precompile += %w/spin.js payment.js email.js/
+Rails.application.config.assets.precompile += %w/spin.js payment.js switch_from_tls.js/
