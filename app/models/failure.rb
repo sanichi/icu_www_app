197773d3835f0b3e34042333bc1dddc2acafcc68
Mark Orr <mark.j.l.orr@googlemail.com>
2014-08-09 13:43:46 +0100

    Make Failure creation more convenient

--- app/models/failure.rb
+++ app/models/failure.rb
@@ -5,21 +5,36 @@ class Failure < ActiveRecord::Base
 
   scope :ordered, -> { order(created_at: :desc) }
 
+  before_create :normalize_details
+
   def self.examine(payload)
-    name, message = payload[:exception]
+    name = payload[:exception].first
     unless IGNORE.include?(name)
-      details = payload.except(:exception).merge(name: name, message: message).map{ |k,v| "#{k}: #{v}" }.sort.join("\n")
-      Failure.create!(name: name, details: details)
+      Failure.create!(name: name, details: payload.dup)
     end
   end
 
   def self.search(params, path)
     matches = ordered
     matches = matches.where("name LIKE ?", "%#{params[:name]}%") if params[:name].present?
+    matches = matches.where("details LIKE ?", "%#{params[:details]}%") if params[:details].present?
     paginate(matches, params, path)
   end
 
   def snippet
-    details.to_s.truncate(50)
+    details.to_s.gsub(/\n/, " ").truncate(50)
+  end
+
+  private
+
+  def normalize_details
+    if details.is_a?(Hash)
+      if details[:exception].is_a?(Array) && details[:exception].size == 2
+        exception = details.delete(:exception)
+        details[:name] = exception.first
+        details[:message] = exception.last
+      end
+      self.details = details.map{ |key,val| "#{key}: #{val}" }.sort.join("\n")
+    end
   end
 end
