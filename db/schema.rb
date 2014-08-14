fd488f0b86397d97a130ccce754f48a5f52df219
Mark Orr <mark.j.l.orr@googlemail.com>
2014-08-14 18:34:05 +0100

    Sync old foreign rating fees

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140809145619) do
+ActiveRecord::Schema.define(version: 20140814155138) do
 
   create_table "articles", force: true do |t|
     t.string   "access",     limit: 20
