ca643a662583ba9502ec39cd1d6c64fc3938c39d
Mark Orr <mark.j.l.orr@googlemail.com>
2014-09-08 21:40:34 +0100

    Revert to the old header setting when signing out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
       user = User.authenticate!(params[:email], params[:password], request.remote_ip)
       session[:user_id] = user.id
       session[:old_locale] = session[:locale] || I18n.default_locale
+      session[:old_hide_header] = !show_header?
       switch_locale(user.locale)
       switch_header(user.hide_header)
       last_page_before_sign_in_or_home("#{t('session.signed_in_as')} #{user.email}")
@@ -17,7 +18,7 @@ class SessionsController < ApplicationController
     if session[:user_id]
       %i[user_id cart_id completed_carts].each { |key| session.delete(key) }
       switch_locale(session.delete(:old_locale))
-      switch_header(false)
+      switch_header(session.delete(:old_hide_header))
       last_page_before_sign_in_or_home(t("session.signed_out"))
     else
       redirect_to switch_to_tls(:sign_in)
