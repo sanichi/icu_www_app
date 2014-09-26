da907b131aac1ec7a014ccd825f874a91836a63c
Mark Orr <mark.j.l.orr@googlemail.com>
2014-09-26 22:36:55 +0100

    Image upload help

--- app/models/global.rb
+++ app/models/global.rb
@@ -29,7 +29,7 @@ module Global
   # A list of help page names.
   HELP_PAGES = %w[
     accounts header index membership pgn privacy profile
-    downloads markdown officers offline_payments shortcuts treasurer
+    downloads images markdown officers offline_payments shortcuts treasurer
   ]
 
   # Validator for full URLs.
